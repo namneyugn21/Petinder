b5677c94cec8943c4a4b727d9fdf1875fb5a9b73
Bao Quoc Phan <phanquocbao2803@gmail.com>
2024-06-01 16:07:54 +0700

    Backend initial code; merge .gitignore into 1 file
    Signed-off-by: Bao Quoc Phan <phanquocbao2803@gmail.com>

---
+++ backend/src/main/java/com/petinder/PetinderApplication.java
@@ -0,0 +1,13 @@
+package com.petinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/petinder/PetinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
