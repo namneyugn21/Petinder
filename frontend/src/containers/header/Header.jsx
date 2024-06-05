48482760c15db3bb5e714bd3538eaa4fd43ee838
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-06-05 00:36:07 -0700

    add @media for responsitivity

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className='about__title'>
                 <h1 className='inter__bold slide-top-title'>Find your sidekick !</h1>
                 <div className='about__introduction ibm-plex-mono-regular slide-top-introduction'>
-                    <p>Welcome to Petinder !<br></br><br></br>You can now find your perfect furry companion is as easy as swiping right! Our web application connects you with adorable pets available for adoption at local shelters.<br></br><br></br> Discover your new best friend today!</p>
+                    <p>Welcome to Petinder,<br></br><br></br>You can now find your perfect furry companion is as easy as swiping right! Our web application connects you with adorable pets available for adoption at local shelters.<br></br><br></br> Discover your new best friend today!</p>
                 </div>
                 <div className='about__button slide-top-button'>
                     <button className='ibm-plex-mono-regular'>Get Started</button>
