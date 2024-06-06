82b0bdaf7efdaf1672550cabae6e8243680a63da
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-06-06 15:55:15 -0700

    small refractor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './containers/home/Home';
 import { Navbar } from './components';
-import Login from './components/login/Login';
 import './App.css';
 
 const App = () => {
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { FcGoogle } from "react-icons/fc";
+import { RiCloseLine, RiFacebookCircleFill } from 'react-icons/ri';
 import './login.css';
+import logo from '../../assets/logo.png';
 
-const Login = () => {
-    return (
-        <div className='login'>
-            Hello World!
-        </div>
-    )
-}
+const Login = ({ isLoginFormVisible, toggleLoginForm, isFadingOut }) => (
+    <>
+        {isLoginFormVisible && (
+            <>
+                <div className={`overlay ${isFadingOut ? 'fade-out' : 'fade-in'}`} onClick={toggleLoginForm}></div>
+                <div className={`navbar__menu-sign-in-form ${isFadingOut ? 'fade-out' : 'fade-in'}`}>
+                    <RiCloseLine id='close-icon' color = '#042c54' size={25} onClick={toggleLoginForm} />
+                    <img src={logo} alt='logo' />
+                    <h2 className='inter__bold'>Get Started !</h2>
+                    <p className='ibm-plex-mono-regular'>By signing in, you agree to our terms and conditions.</p>
+                    <div className='navbar__menu-sign-in-form__icon-container'>
+                        <div className='navbar__menu-sign-in-form__icon-inner-container'>
+                            <FcGoogle />
+                        </div>
+                        <span className='inter__medium'>Google Account</span>
+                    </div>
+                    <div className='navbar__menu-sign-in-form__icon-container '>
+                        <div className='navbar__menu-sign-in-form__icon-inner-container'>
+                            <RiFacebookCircleFill color = '#042c54' />
+                        </div>
+                        <span className='inter__medium'>Facebook</span>
+                    </div>
+                </div>
+            </>
+        )}
+
+    </>
+);
 
 export default Login;
