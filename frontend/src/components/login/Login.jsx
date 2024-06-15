db701a0663ef83219cc3cf0efacc2bb7993f4b49
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-06-15 12:27:31 -0700

    branch init

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -5,6 +5,19 @@ import './login.css';
 import logo from '../../assets/logo.png';
 
 const Login = ({ isLoginFormVisible, toggleLoginForm, isFadingOut }) => {
+    const handleGoogleSignIn = () => {
+        // Call the webhook for Google OAuth2 authorization
+        fetch('http://localhost:8086/oauth2/authorization/google?redirect_uri=http://localhost:3000/')
+            .then(response => {
+                // Handle the response
+                console.log(response);
+            })
+            .catch(error => {
+                // Handle errors
+                console.error(error);
+            });
+    };
+    
     return (
         <>
             {isLoginFormVisible && (
@@ -15,7 +28,7 @@ const Login = ({ isLoginFormVisible, toggleLoginForm, isFadingOut }) => {
                         <img src={logo} alt='logo' />
                         <h2 className='inter__bold'>Get Started !</h2>
                         <p className='ibm-plex-mono-regular'>By signing in, you agree to our terms and conditions.</p>
-                        <button className='navbar__menu-sign-in-form__icon-container'>
+                        <button className='navbar__menu-sign-in-form__icon-container' onClick={handleGoogleSignIn}>
                             <div className='navbar__menu-sign-in-form__icon-inner-container'>
                                 <FcGoogle />
                             </div>
