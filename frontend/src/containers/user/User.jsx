e8a680fd48c59977d9ade1a5f6b76e7eeba096e4
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-07-21 13:35:45 -0700

    css fixed

--- frontend/src/containers/user/User.jsx
+++ frontend/src/containers/user/User.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect } from 'react'
 import Navbar from '../../components/navbar/Navbar'
-import '../../containers/user/user.css'
+import '../user/user.css'
 
 const User = () => {
     const [firstName, setFirstName] = useState('');
