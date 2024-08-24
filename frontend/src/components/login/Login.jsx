7e1a38b3bad20f0a2e62d4eaa9cfcfb78c864a82
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-08-23 20:48:09 -0700

    fix redirect

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -19,7 +19,7 @@ const Login = ({ isLoginFormVisible, toggleLoginForm, isFadingOut }) => {
                             <div className='navbar__menu-sign-in-form__icon-inner-container'>
                                 <FcGoogle />
                             </div>
-                            <a className='inter__medium' href='http://petinder.bao2803.co/auth/oauth2/authorization/google?redirect_uri=https://petinder-green.vercel.app/success'>Google Account</a>
+                            <a className='inter__medium' href='http://petinder.bao2803.co:3000/auth/oauth2/authorization/google?redirect_uri=http://petinder.bao2803.co:3000/success'>Google Account</a>
                         </button>
                         <div className='navbar__menu-sign-in-form__icon-container '>
                             <div className='navbar__menu-sign-in-form__icon-inner-container'>
